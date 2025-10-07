f0720bf70724f011069df0cfe9e169a33bb5757f
pauytrh2 <pauytrh@gmail.com>
2025-10-07 11:44:51 +0300

    Clean up player code

--- addons/proto_controller/player.gd
+++ addons/proto_controller/player.gd
@@ -1,58 +1,35 @@
-# ProtoController v1.0 by Brackeys
-# CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
-
 extends CharacterBody3D
 
-## Can we move around?
 @export var can_move : bool = true
-## Are we affected by gravity?
 @export var has_gravity : bool = true
-## Can we press to jump?
 @export var can_jump : bool = true
-## Can we hold to run?
 @export var can_sprint : bool = false
-## Can we press to enter freefly mode (noclip)?
 @export var can_freefly : bool = false
 
 @export_group("Speeds")
-## Look around rotation speed.
 @export var look_speed : float = 0.002
-## Normal speed.
 @export var base_speed : float = 7.0
-## Speed of jump.
 @export var jump_velocity : float = 4.5
-## How fast do we run?
 @export var sprint_speed : float = 10.0
-## How fast do we freefly?
 @export var freefly_speed : float = 25.0
 
 @export_group("Input Actions")
-## Name of Input Action to move Left.
 @export var input_left : String = "ui_left"
-## Name of Input Action to move Right.
 @export var input_right : String = "ui_right"
-## Name of Input Action to move Forward.
 @export var input_forward : String = "ui_up"
-## Name of Input Action to move Backward.
 @export var input_back : String = "ui_down"
-## Name of Input Action to Jump.
 @export var input_jump : String = "ui_accept"
-## Name of Input Action to Sprint.
 @export var input_sprint : String = "sprint"
-## Name of Input Action to toggle freefly mode.
 @export var input_freefly : String = "freefly"
+
+@onready var head: Node3D = $Head
+@onready var collider: CollisionShape3D = $Collider
+@onready var ray_cast_3d: RayCast3D = $Head/Camera3D/RayCast3D
 
 var mouse_captured : bool = false
 var look_rotation : Vector2
 var move_speed : float = 0.0
 var freeflying : bool = false
-
-## IMPORTANT REFERENCES
-@onready var head: Node3D = $Head
-@onready var collider: CollisionShape3D = $Collider
-@onready var ray_cast_3d: RayCast3D = $Head/Camera3D/RayCast3D
 
 const DIRT_BLOCK: int = 1
 
@@ -62,49 +39,36 @@ func _ready() -> void:
     look_rotation.x = head.rotation.x
 
 func _unhandled_input(event: InputEvent) -> void:
-    # Mouse capturing
     if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
         capture_mouse()
     if Input.is_key_pressed(KEY_ESCAPE):
         release_mouse()
 
-    # Look around
     if mouse_captured and event is InputEventMouseMotion:
         rotate_look(event.relative)
 
-    # Toggle freefly mode
     if can_freefly and Input.is_action_just_pressed(input_freefly):
-        if not freeflying:
-            enable_freefly()
-        else:
+        if freeflying:
             disable_freefly()
+        else:
+            enable_freefly()
 
 func _physics_process(delta: float) -> void:
-    # If freeflying, handle freefly and nothing else
-    if can_freefly and freeflying:
-        var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-        var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-        motion *= freefly_speed * delta
-        move_and_collide(motion)
+    if freeflying and can_freefly:
+        freefly_process(delta)
         return
 
-    # Apply gravity to velocity
-    if has_gravity:
-        if not is_on_floor():
+    if not is_on_floor() and has_gravity:
             velocity += get_gravity() * delta
 
-    # Apply jumping
-    if can_jump:
-        if Input.is_action_just_pressed(input_jump) and is_on_floor():
+    if Input.is_action_just_pressed(input_jump) and is_on_floor() and can_jump:
             velocity.y = jump_velocity
 
-    # Modify speed based on sprinting
-    if can_sprint and Input.is_action_pressed(input_sprint):
-            move_speed = sprint_speed
+    if Input.is_action_pressed(input_sprint) and can_sprint:
+        move_speed = sprint_speed
     else:
         move_speed = base_speed
 
-    # Apply desired movement to velocity
     if can_move:
         var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
         var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -126,13 +90,9 @@ func _physics_process(delta: float) -> void:
         if ray_cast_3d.is_colliding() and ray_cast_3d.get_collider().has_method("place_block"):
                 ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + ray_cast_3d.get_collision_normal(), DIRT_BLOCK)
 
-    # Use velocity to actually move
     move_and_slide()
 
 
-## Rotate us to look around.
-## Base of controller rotates around y (left/right). Head rotates around x (up/down).
-## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
 func rotate_look(rot_input : Vector2):
     look_rotation.x -= rot_input.y * look_speed
     look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
@@ -147,6 +107,7 @@ func enable_freefly():
     collider.disabled = true
     freeflying = true
     velocity = Vector3.ZERO
+
 
 func disable_freefly():
     collider.disabled = false
@@ -163,8 +124,13 @@ func release_mouse():
     mouse_captured = false
 
 
-## Checks if some Input Actions haven't been created.
-## Disables functionality accordingly.
+func freefly_process(delta: float):
+        var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
+        var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+        motion *= freefly_speed * delta
+        move_and_collide(motion)
+
+
 func check_input_mappings():
     if can_move and not InputMap.has_action(input_left):
         push_error("Movement disabled. No InputAction found for input_left: " + input_left)
